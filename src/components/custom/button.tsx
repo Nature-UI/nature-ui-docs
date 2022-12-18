71d8b7ecfcda15992822fcb7655dafa900291efc
Divine Hycenth <divinehycenth@outlook.com>
2022-12-18 13:09:46 +0100

    #81 removed button variants and fix alert color

--- src/components/custom/button.tsx
+++ src/components/custom/button.tsx
@@ -1,10 +1,7 @@
-import { Button as NatureButton } from '@nature-ui/core';
+import { Button as NatureButton } from "@nature-ui/core";
 
 const Button = (props) => (
-	<NatureButton
-		{...props}
-		className={`bg-primary-500 text-white ${props.className}`}
-	/>
+  <NatureButton {...props} className={`text-white ${props.className}`} />
 );
 
 export default Button;
